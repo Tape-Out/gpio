9f4ffc9127de245d8266fb1c82be420f5819d928
heke1228 <chengkelfan@qq.com>
2025-11-26 17:16:30 +0800

    ...

--- gpio_mmio.v
+++ gpio_mmio.v
@@ -109,8 +109,7 @@ module gpio_mmio #(
     always @(posedge clk) begin
         if (!resetn) begin
             mem_ready <= 0;
-        end
-        mem_ready <= mem_valid && !mem_instr;
+        end else mem_ready <= mem_valid && !mem_instr;
     end
 
     always @(posedge clk) begin: MMIO_READ
